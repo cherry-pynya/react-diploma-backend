3aa97b3f2baa0674424d72e6151bf785bae6ecd3
chery-pynya <nikita.cherepnya@gmail.com>
2021-10-06 18:12:31 +0300

    delay and fortune added

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ const randomNumber = (start, stop) => {
 
 const fortune = (ctx, body = null, status = 200) => {
     // Uncomment for delay
-    // const delay = randomNumber(1, 10) * 1000;
-    const delay = 0;
+    const delay = randomNumber(1, 10) * 1000;
+    //const delay = 0;
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            // Uncomment for error generation
-            // if (Math.random() > 0.8) {
-            //     reject(new Error('Something bad happened'));
-            //     return;
-            // }
+            //Uncomment for error generation
+            if (Math.random() > 0.95) {
+                reject(new Error('Something bad happened'));
+                return;
+            }
 
             ctx.response.status = status;
             ctx.response.body = body;
@@ -59,6 +59,8 @@ router.get('/api/categories', async (ctx, next) => {
 
 router.get('/api/items', async (ctx, next) => {
     const { query } = ctx.request;
+    console.log(query);
+    console.log(query.q)
 
     const categoryId = query.categoryId === undefined ? 0 : Number(query.categoryId);
     const offset = query.offset === undefined ? 0 : Number(query.offset);
@@ -69,7 +71,7 @@ router.get('/api/items', async (ctx, next) => {
         .filter(o => o.title.toLowerCase().includes(q) || o.color.toLowerCase() === q)
         .slice(offset, offset + moreCount)
         .map(itemBasicMapper);
-
+    console.log(filtered);
     return fortune(ctx, filtered);
 });
 
